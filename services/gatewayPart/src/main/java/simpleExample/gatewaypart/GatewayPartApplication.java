b8799c3455d5e6ad638a05744586cba7de08e840
SaraAdelAly <saraadelaly97@gmail.com>
2024-08-22 23:07:54 +0300

    servericesgateway

---
+++ services/gatewayPart/src/main/java/simpleExample/gatewaypart/GatewayPartApplication.java
@@ -0,0 +1,14 @@
+package simpleExample.gatewaypart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class GatewayPartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayPartApplication.class, args);
+	}
+
+}
---
+++ services/gatewayPart/src/test/java/simpleExample/gatewaypart/GatewayPartApplicationTests.java
@@ -0,0 +1,13 @@
+package simpleExample.gatewaypart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayPartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
